76b0be8d4a8601c0336e85743222a0ecf4e1ddad
DavidIZV <drodriguez112@ieszaidinvergeles.org>
2021-12-22 12:08:00 +0100

    Fix autoloader error

--- autoload.php
+++ autoload.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/core/BucketClient.php';
 require_once __DIR__ . '/core/Uploader.php';
 require_once __DIR__ . '/core/Util.php';
