184b5ad3909659b47a1677d5c8f6be892e3f357f
DavidIZV <drodriguez112@ieszaidinvergeles.org>
2022-01-09 12:16:35 +0100

    Change visual to select image

--- bucket-vista.php
+++ bucket-vista.php
@@ -1,13 +1,13 @@
 <?php
-$filesInDirectory = array ();
+$filesInDirectory = array();
 $dir = './originales';
-$cdir = scandir ( $dir );
-foreach ( $cdir as $key => $value ) {
-	if ($value != '..' and $value != '.') {
-		if (! is_dir ( $dir . DIRECTORY_SEPARATOR . $value )) {
-			array_push ( $filesInDirectory, $value );
-		}
-	}
+$cdir = scandir($dir);
+foreach ($cdir as $key => $value) {
+    if ($value != '..' and $value != '.') {
+        if (! is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
+            array_push($filesInDirectory, $value);
+        }
+    }
 }
 ?>
 <!doctype html>
@@ -22,15 +22,13 @@ foreach ( $cdir as $key => $value ) {
 	<section>
 		<div class="vertical">
 			<h3>Choose an image to upload to your Bucket</h3>
-			<?php
-			foreach ( $filesInDirectory as $value ) {
-				?>
-			<p>
-				<a href="./bucket-subir.php?toupload=<?=$value?>"><?=$value?></a>
-			</p>
-			<?php
-			}
-			?>
+			<div class="three-positions">
+    			<?php foreach ( $filesInDirectory as $value ) { ?>
+    				<a href="./bucket-subir.php?toupload=<?=$value?>">
+    					<img class="img-500" src="./originales/<?=$value?>" />
+    				</a>
+    			<?php }	?>
+			</div>
 		</div>
 	</section>
 
