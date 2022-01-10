10baf18ccc7cf8df1f9ce33caed02e5e5652ee22
David <drodriguez112@ieszaidinvergeles.org>
2022-01-10 21:56:42 +0000

    First version with Jcrop

---
+++ statics/aws-jcrop.js
@@ -0,0 +1,37 @@
+(function () {
+	'use strict';
+	
+// 	window.awsjcrop=Jcrop.attach('myimage');
+//   window.awsjcrop.setOptions({ multi: true, shadeColor: 'red', aspectRatio: 1 });
+
+var jcp;
+      Jcrop.load('myimage').then(img => {
+        jcp = Jcrop.attach(img,{multi:true});
+        const rect = Jcrop.Rect.sizeOf(jcp.el);
+        // jcp.newWidget(rect.scale(.7,.5).center(rect.w,rect.h));
+        jcp.focus();
+      });
+
+      function setImage(tag){
+        document.getElementById('target').src =
+          'https://d3o1694hluedf9.cloudfront.net/'+tag;
+      }
+
+      function rcoord() {
+        const w = jcp.el.offsetWidth;
+        const h = jcp.el.offsetHeight;
+        return [Math.round(Math.random()*w), Math.round(Math.random()*h)];
+      }
+
+      function rrect() {
+        return Jcrop.Rect.fromCoords(rcoord(),rcoord());
+      }
+
+      function anim(){
+        if (!jcp.active) return false;
+        const animtype = document.getElementById('animtype').value;
+        jcp.active.animate(rrect(),null,animtype);
+        jcp.focus();
+      }
+	
+})();
--- view-bucket-analizar.php
+++ view-bucket-analizar.php
@@ -24,6 +24,8 @@ $faces = Mapper::translateDataAws($analysis);
 <meta charset="UTF-8">
 <title>Uploaded</title>
 <link rel="stylesheet" href="./style.css">
+<link rel="stylesheet" href="https://unpkg.com/jcrop/dist/jcrop.css">
+<script src="https://unpkg.com/jcrop" type="text/javascript"></script>
 </head>
 <body>
 	
@@ -33,13 +35,18 @@ $faces = Mapper::translateDataAws($analysis);
 		<div class="vertical">
 			<h3>Image to analyze</h3>
 			<div class="all-screen">
-				<img class="img-max-size" src="./originales/<?=$imageName?>" />
+				<img id="myimage" class="img-max-size" src="./originales/<?=$imageName?>" />
 			</div>
 		</div>
 		<div class="vertical right-side">
 			<p><?=var_dump($faces[0]->array())?></p>
 		</div>
 	</section>
+	
+	<div class="none">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript"></script>
+		<script src="./statics/aws-jcrop.js" type="text/javascript"></script>
+	</div>
 
 </body>
 </html>
