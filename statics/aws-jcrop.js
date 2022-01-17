a9e9a92e8e25a4ce3d685331685b1ad06d3decfd
David <drodriguez112@ieszaidinvergeles.org>
2022-01-17 18:51:43 +0000

    Change ajax to fetch class

--- statics/aws-jcrop.js
+++ statics/aws-jcrop.js
@@ -1,31 +1,41 @@
 (function() {
 	'use strict';
 
-	var jcp, rect;
+	let jcp, rect;
 	Jcrop.load('myimage').then(img => {
 		jcp = Jcrop.attach(img, { multi: true });
 		rect = Jcrop.Rect.sizeOf(jcp.el);
 		jcp.focus();
 	});
 
-	var img = jQuery("#myimage");
+	let img = jQuery("#myimage");
 
-	var imageName = img.attr('class').split(" ");
+	let imageName = img.attr('class').split(" ");
 
-	$.getJSON("/pia/upload/logic/bucket-analizar.php?toanalyze=" + imageName[0], function(result) {
+	fetch("https://informatica.ieszaidinvergeles.org:10056/pia/upload/logic/bucket-analizar.php?toanalyze=" + imageName[0], {
+		method: 'GET',
+	})
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data) {
+			console.log('Request succeeded with JSON response', data);
+			$(".aws-data").text(JSON.stringify(data.faces, null, 2));
 
-		$(".aws-data").text(JSON.stringify(result.faces, null, 2));
-
-		result.faces.forEach(function(face) {
-			var t = img[0].height * face.top;
-			var l = img[0].width * face.left;
-			var h = img[0].height * face.height;
-			var w = img[0].width * face.width;
-			const rect = Jcrop.Rect.create(l, t, w, h);
-			const options = {};
-			jcp.newWidget(rect, options);
+			data.faces.forEach(function(face) {
+				var t = img[0].height * face.top;
+				var l = img[0].width * face.left;
+				var h = img[0].height * face.height;
+				var w = img[0].width * face.width;
+				const rect = Jcrop.Rect.create(l, t, w, h);
+				const options = {
+					opacity: 0.7
+				};
+				jcp.newWidget(rect, options);
+			});
+		})
+		.catch(function(error) {
+			console.log('Request failed', error);
 		});
-
-	});
 
 })();
